f5af60b98446c911ec1f168574597d6a8e661a29
kcheykim <kosalcheykim@gmail.com>
2022-02-10 11:59:17 -0800

    adding queries and mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -23,7 +23,23 @@ const typeDefs = gql`
     token: ID!
     user: User
   }
+  
+  type Query {
+    me: User
+  }
 
+  type bookInput {
+    author: [String]
+    description
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+
+    saveBook(book: Book ): User
+    removeBook(bookId: String!): User
+  }
 `;
 
 module.exports = typeDefs;
