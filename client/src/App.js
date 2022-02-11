9b2cdf4ebfc4298f6e0f4ae48afe316090dbb78e
kcheykim <kosalcheykim@gmail.com>
2022-02-10 23:53:35 -0800

    changes to the app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+
+const httpLink = createHttpLink({ uri: '/graphql', });
+
+const client = new ApolloClient({ link: httpLink, cache: new InMemoryCache(), });
 
 function App() {
   return (
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,27 +6,25 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) { token = token.split(' ').pop().trim(); }
-
-    if (!token) {return req;}
-    //{ return res.status(400).json({ message: 'You have no token!' }); }
-
-    // verify token and get user data out of it
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      //console.log('Invalid token');
-      return res.status(400).json({ message: 'Invalid token!' });
+    // separate "Bearer" from "<tokenvalue>"
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
     }
 
-    // send to next endpoint
-    //next();
+    // if no token, return request object as is
+    if (!token) { return req; }
+
+    try {
+      // decode and attach user data to request object
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch { console.log('Invalid token'); }
+
+    // return updated request object
     return req;
   },
   signToken: function ({ username, email, _id }) {
